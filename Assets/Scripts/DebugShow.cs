c3e35ec3540b1d58e4070301885cd1bb2a534381
sakata1024 <skt97yuma@outlook.jp>
2021-05-02 11:23:23 +0900

    勉強コミット

---
+++ Assets/Scripts/DebugShow.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugShow : MonoBehaviour
+{
+    public float speedX = 0.001f;
+    private Color col = Color.red;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("starting");
+        int a = 123;
+        a = 456;
+        float b = 123.0f;
+        string c = "hello";
+        bool d = true;
+        GameObject g = gameObject;
+        Debug.Log(g.name);
+        cam = gameObject.GetComponent<Camera>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(speedX, 0f, 0f);
+        /*
+        float h, s, v = 0f;
+        Color.RGBToHSV(col, out h, out s, out v);
+        h = h + 0.001f;
+        col = Color.HSVToRGB(h, s, v);
+        cam.backgroundColor = col
+        */
+    }
+
+    public void Click()
+    {
+        Debug.Log("‰Ÿ‚·‚È");
+    }
+    
+}
